891280d49a68cde8435b76e9c4f30434e179bda1
Tinguaro <tinguaro@tinguaro-portatil.(none)>
2013-04-13 18:05:52 +0100

    Subir archivos

---
+++ js/tokens.js
@@ -0,0 +1,110 @@
+"use strict";
+// tokens.js
+// 2010-02-23
+
+// Produce an array of simple token objects from a string.
+// A simple token object contains these members:
+//      type: 'name', 'string', 'number', 'operator'
+//      value: string or number value of the token
+//      from: index of first character of the token
+//      to: index of the last character + 1
+
+// Comments are ignored.
+
+RegExp.prototype.bexec = function(str) {
+  var i = this.lastIndex;
+  var m = this.exec(str);
+  if (m && m.index == i) return m;
+  return null;
+}
+
+String.prototype.tokens = function () {
+    var from;                   // The index of the start of the token.
+    var i = 0;                  // The index of the current character.
+    var n;                      // The number value.
+    var str;                    // The string value.
+    var m;                      // Matching
+    var result = [];            // An array to hold the results.
+
+    var WHITES              = /\s+/g;
+    var ID                  = /[a-zA-Z_]\w*/g;
+    var NUM                 = /\b\d+(\.\d*)?([eE][+-]?\d+)?\b/g;
+    var STRING              = /('\\.|[^'])*'|"(\\.|[^"])*"/g;
+    var ONELINECOMMENT      = /[/][/].*/g;
+    var MULTIPLELINECOMMENT = /\/[*](.|\n)*?[*]\//g;
+    var TWOCHAROPERATORS    = /([+][+=]|-[-=]|=[=<>]|[<>][=<>]|&&|[|][|])/g;
+    var ONECHAROPERATORS    = /([-+*\/=<>()&|:;[\]])/g;
+
+    // Make a token object.
+    var make = function (type, value) {   //crea objetos token
+        return {
+            type: type,
+            value: value,
+            from: from,   //'from' y 'to' se sacan de variables que estan en ambito
+            to: i
+        };
+    };
+
+    // Begin tokenization. If the source string is empty, return nothing.
+    if (!this) return; 
+
+    // Loop through this text
+    while (i < this.length) {
+        WHITES.lastIndex =  ID.lastIndex = NUM.lastIndex = STRING.lastIndex =
+        ONELINECOMMENT.lastIndex = MULTIPLELINECOMMENT.lastIndex =
+        TWOCHAROPERATORS.lastIndex = ONECHAROPERATORS.lastIndex = i; //Todas son iguales a donde acabó el matching
+        from = i; //Cursor que movemos por la cadena
+        
+        // Ignore whitespace.
+        if (m = WHITES.bexec(this)) {
+            str = m[0];
+            i = i + str.length;
+            
+        // name.
+        } else if (m = ID.bexec(this)) {
+            str = m[0];
+            i = i + str.length; //avanzar índice i
+            result.push(make('name', str));
+
+        // number.
+        } else if (m = NUM.bexec(this)) {
+            str = m[0];
+            i = i + str.length;
+            n = +str; //Forzar conversión de cadena a número
+            if (isFinite(n)) {
+                result.push(make('number', n));
+            } else {
+                make('number', str).error("Bad number");
+            }
+            
+        // string
+        } else if (m = STRING.bexec(this)) {
+            str = m[0];
+            i = i + str.length;
+            str = str.replace(/^["']/,'');  
+            str = str.replace(/["']$/,'');
+            result.push(make('string', str));
+            
+        // comment.
+        } else if ((m = ONELINECOMMENT.bexec(this))  || 
+                   (m = MULTIPLELINECOMMENT.bexec(this))) {
+            str = m[0];
+            i = i + str.length;
+        
+        // two char operator
+        } else if (m = TWOCHAROPERATORS.bexec(this)) {
+            str = m[0];
+            i = i + str.length;
+            result.push(make('operator', str));
+        
+        // single-character operator
+        } else if (m = ONECHAROPERATORS.bexec(this)){
+            result.push(make('operator', this.substr(i,1)));
+            i = i + str.length;
+        } else {
+          throw "Syntax error near '"+this.substr(i)+"'";
+        }
+    }
+    return result;
+};
+
